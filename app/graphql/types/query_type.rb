f7ccd4c1edfe01202319daa5bedfe8c271310cf1
Ricky Dam <RickyDamOttawa@gmail.com>
2020-03-01 20:36:47 -0500

    Register a field called items and add its resolver method

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,7 +2,13 @@ module Types
   class QueryType < Types::BaseObject
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
+    field :items,
+          [Types::ItemType],
+          null: false,
+          description: "Return a list of items in the martian library"
 
+    def items
+      Item.all
     end
   end
 end
