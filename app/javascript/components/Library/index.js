f6c3f918242a36934219e3f5dbb73c7e610082be
Ricky Dam <RickyDamOttawa@gmail.com>
2020-03-03 21:01:23 -0500

    Create the Library React component

---
+++ app/javascript/components/Library/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const LibraryQuery = gql`
+    {
+        items {
+            id
+            title
+            user {
+                email
+            }
+        }
+    }
+`;
+
+export default() => (
+    <Query query={LibraryQuery}>
+        {({ data, loading }) => (
+            <div>
+                {loading ? 'loading...' : data.items.map(({ title, id, user }) => (
+                    <div key={id}>
+                        <strong>{title}</strong> {user ? `added by ${user.email}` : null}
+                    </div>
+                ))}
+            </div>
+        )}
+    </Query>
+);
